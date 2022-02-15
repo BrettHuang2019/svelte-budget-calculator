eae16aafeb216a0993f1e7845a70f825fe033ee2
BrettHuang <Brett@EdokiAcademy.com>
2022-02-14 21:44:07 -0500

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<!-- Javascript / Logic -->
+<script>
+	import Navbar from './Navbar.svelte';
+	import Title from './Title.svelte';
+</script>
+
+<!-- CSS / Styling -->
+
+<!-- HTML -->
+<Navbar />
+<Title title="add expense" />
+<Title title="expense list" />
+<Title />
